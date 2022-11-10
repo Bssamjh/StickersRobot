79a0488805b8c30621270bac5764f07f9e0c4965
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-10 22:41:47 +0530

    Add files via upload

---
+++ AltronRobot/config.py
@@ -0,0 +1,42 @@
+class Config(object):
+    LOGGER = True
+
+    API_ID = 
+    API_HASH = ""
+    TOKEN = ""
+    OWNER_ID = 
+    OWNER_USERNAME = ""
+    SUPPORT_CHAT = ""
+    JOIN_LOGGER = ()
+    EVENT_LOGS = ()
+
+    SQLALCHEMY_DATABASE_URI = ""
+    MONGO_DB_URI = ""
+    LOAD = []
+    NO_LOAD = ["rss"]
+    INFOPIC = True
+
+    # OPTIONAL
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = 
+
+    DEL_CMDS = True
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BL_CHATS = []
+    SPAMMERS = None
+    START_IMG = ""
+    ARQ_API_KEY = "LJMETG-DPHBCX-DGHJCD-TMFIGB-ARQ"
+    ARQ_API_URL = "https://arq.hamker.in"
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
